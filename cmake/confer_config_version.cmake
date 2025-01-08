74d8d6140f466bb39669c44d9fc0b7aa4a8e9e3f
Anstro Pleuton <anstropleuton@github.com>
2025-01-08 02:58:23 -0800

    Make CMake files more reusable

--- cmake/confer_config_version.cmake
+++
@@ -1,8 +0,0 @@
-# confer_config.cmake
-include(${CMAKE_CURRENT_LIST_DIR}/confer_targets.cmake)
-
-install(FILES
-    ${CMAKE_CURRENT_SOURCE_DIR}/cmake/confer_config.cmake
-    ${CMAKE_CURRENT_BINARY_DIR}/confer_config_version.cmake
-    DESTINATION ${CMAKE_INSTALL_DATADIR}/confer
-)
